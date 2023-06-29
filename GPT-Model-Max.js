f891a432ded14be8c2720665be3d9862095663e1
BlueSkyXN <63384277+BlueSkyXN@users.noreply.github.com>
2023-06-29 11:10:56 +0800

    1.1.1

--- GPT-Model-Max.js
+++ GPT-Model-Max.js
@@ -3,7 +3,7 @@
 // @namespace   BlueSkyXN
 // @match       *://chat.openai.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1.1
 // @author      BlueSkyXN
 // @description https://github.com/BlueSkyXN/GPT-Models-Plus 用于在ChatGPT 网页版自选或强制锁定模型，支持GPT3.5/GPT4/GPT3.5M/GPT4M/GPT4B/GPT4P等模型，用油猴脚本TamperMonkey加载
 // @license     GPLv3 and MIT
@@ -24,7 +24,11 @@
       { value: 'gpt-4-mobile', label: 'GPT-4 Mobile' },
       { value: 'gpt-4', label: 'GPT-4' },
       { value: 'gpt-4-browsing', label: 'GPT-4 Browsing' },
-      { value: 'gpt-4-plugins', label: 'GPT-4 Plugins' }
+      { value: 'gpt-4-plugins', label: 'GPT-4 Plugins' },
+      { value: 'gpt-4-code-interpreter', label: 'GPT-4 Coding' },
+      { value: 'text-davinci-002-render-sha-code-interpreter', label: 'GPT-3.5 Coding' },
+      { value: 'text-davinci-002-render-sha-browsing', label: 'GPT-3.5 Browsing' },
+      { value: 'text-davinci-002-render-sha-plugins', label: 'GPT-3.5 Plugins' }
   ]
 
   const origFetch = window.fetch
